b0c4c1e6d9f4e8f50e4d08342663b5f1b053f823
Aucdmn <auop129@163.com>
2020-10-28 16:21:01 +0800

    developing...
    包含：计时器、登录控制展示

---
+++ my-app/src/index.jsx
@@ -0,0 +1,156 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+// 计时器
+// function tick() {
+//   const element = (
+//       <div>
+//           <h1>Hello, world!</h1>
+//           <h2>It is {new Date().toLocaleTimeString()}.</h2>
+//       </div>
+//   );
+//   ReactDOM.render(element, document.getElementById('root'));
+// }
+
+// setInterval(tick, 1000);
+// ----------------------------------------------------------------
+
+// 测试 props 功能
+// function Welcome(props) {
+//   return <h1>Hello, {props.name}</h1>
+// }
+
+// function App() {
+//   return (
+//     <div>
+//       <Welcome name = "Mihara" />
+//       <Welcome name = "Yohji" />
+//       <Welcome name = "Nigel" />
+//     </div>
+//   )
+// }
+
+// 测试 props 功能
+// ReactDOM.render(
+//   <App />,
+//   document.getElementById("root")
+// );
+// ----------------------------------------------------------------
+
+// 时钟展示器
+// class Clock extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {date: new Date()};
+//   }
+
+//   componentDidMount() {
+//     this.timerID = setInterval(
+//       () => this.tick(),
+//       1000
+//     );
+//   }
+
+//   componentWillUnmount() {
+//     clearInterval(this.timerID);
+//   }
+
+//   tick() {
+//     this.setState({
+//       date: new Date()
+//     });
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <h1>Hell No</h1>
+//         <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+//       </div>
+//     );
+//   }
+// }
+// 时钟展示器
+// ReactDOM.render(
+//   <Clock />,
+//   document.getElementById('root')
+// );
+// ----------------------------------------------------------------
+
+// 登录控制展示
+class LoginControl extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleLoginClick = this.handleLoginClick.bind(this);
+    this.handleLogoutClick = this.handleLogoutClick.bind(this);
+    this.state = {isLoggedIn: false};
+  }
+
+  handleLoginClick() {
+    this.setState({isLoggedIn: true});
+  }
+
+  handleLogoutClick() {
+    this.setState({isLoggedIn: false});
+  }
+
+  render() {
+    const isLoggedIn =  this.state.isLoggedIn;
+    let button;
+
+    if(isLoggedIn) {
+      button = <LogoutButton onClick = {this.handleLogoutClick} />;
+    } else {
+      button = <LoginButton onClick = {this.handleLoginClick} />;
+    }
+
+    return (
+      <div>
+        <Greeting isLoggedIn = {isLoggedIn} />
+        {button}
+      </div>
+    );
+  }
+}
+
+function UserGreeting(props) {
+  return <h1>Welcome back!</h1>;
+}
+
+function GuestGreeting(props) {
+  return <h1>Please sign up.</h1>;
+}
+
+function Greeting(props) {
+  const isLoggedIn = props.isLoggedIn;
+  if (isLoggedIn) {
+    return <UserGreeting />;
+  }
+  return <GuestGreeting />;
+}
+
+function LoginButton(props) {
+  return (
+    <button onClick = {props.onClick}>
+      Login
+    </button>
+  );
+}
+
+function LogoutButton(props) {
+  return (
+    <button onClick = {props.onClick}>
+      Logout
+    </button>
+  );
+}
+
+ReactDOM.render(
+  <LoginControl />,
+  document.getElementById('root')
+);
+
+
+
+
