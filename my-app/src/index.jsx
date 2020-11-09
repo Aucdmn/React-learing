2d12b5909924faf03fe274d75bc4ec2ad1b82ddb
Aucdmn <auop129@163.com>
2020-11-09 19:03:53 +0800

    组合和继承

--- my-app/src/index.jsx
+++ my-app/src/index.jsx
@@ -196,4 +196,57 @@ import './index.css';
 //   document.getElementById("root")
 // );
 
-s
+// function FancyBorder(props) {
+//     return (
+//         <div className={'FancyBorder FancyBorder-' + props.color}>
+//             {props.children}
+//         </div>
+//     )
+// }
+
+// function WelcomeDialog() {
+//     return (
+//         <FancyBorder color="blue">
+//             <h1 className="Dialog-title">
+//                 Welcome
+//             </h1>
+//             <p className="Dialog-message">
+//                 Thank you for visiting our spacecraft!
+//             </p>
+//         </FancyBorder>
+//     )
+// }
+
+// ReactDOM.render(
+//     <WelcomeDialog />,
+//     document.getElementById('root')
+// )
+
+
+function Dialog(props) {
+    return (
+        <div className="dialog">
+            <div className="dialog-title">
+                {props.title}
+            </div>
+            <div className="dialog-content">
+                {props.content}
+            </div>
+            {props.children}
+        </div>
+    )
+}
+
+function WelcomeContent() {
+    return (
+        <Dialog title="NiHao" 
+                content="HHHHH">
+        <input ></input>
+        </Dialog>
+    )
+}
+
+ReactDOM.render(
+    <WelcomeContent />,
+    document.getElementById('root')
+)
